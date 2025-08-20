7386141147e75814a14dda704fa6143d9546ed59
kcafernandes <kcafernandes06@gmail.com>
2025-08-20 01:42:59 -0700

    added detective decion prompt Closes #20

--- src/main.cpp
+++ src/main.cpp
@@ -52,4 +52,33 @@ int main(){
     type("|            THE GRIMSHIRE MANOR MURDER           |\n", 2);
     type("|=================================================|\n\n", 2);
 
+    // ask the player a question
+    type("Are you up to the task, detective?\n\n");
+    cout << "A) Yes\nB) No\n";
+    cout << "Enter your choice: ";
+
+    // responses to the selected choice
+    string choice;
+    bool choiceMade = false;
+    while(choiceMade == false){
+        getline(cin, choice);
+        if(choice == "a" || choice == "A" || choice == "yes" || choice == "Yes") {
+            choiceMade = true;
+            type("\nVery Well. The fate of the investigation now rests with you.\n");
+            type("Tread carefully… every word, every glance, every clue matters.\n");
+            type("Let us begin.\n");
+        } else if(choice == "b" || choice == "B" || choice == "no" || choice == "No") {
+            choiceMade = true;
+            type("\n…Ah. Perhaps the weight of the Grimshire legacy is too much for one soul to bear.\n");
+            type("But justice waits for no one, detective.\n");
+            type("When you are ready… the manor will still be here.\n");
+        } else {
+            choiceMade = false;
+            type("\nI didn’t quite catch that…\n");
+            type("Is it a yes… or a no, detective?\n");
+            type("The Grimshires don’t take kindly to indecision…\n");
+            cout << endl;
+            cout << "Enter your choice: ";
+        }
+    }
 }
