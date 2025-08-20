7329901d5f217a890ed586711f2ac012c1f36c83
kcafernandes <kcafernandes06@gmail.com>
2025-08-19 17:35:22 -0700

    Add typewriter effect

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+
+// made a function to type slowly like a typewriter, instead of the speedy cout
+// makes it more ominous..? suspensful..? trust
+void type(string text, int speed = 30){
+    for(int i = 0; i < text.length(); i++){
+        cout << text[i] << flush;
+        this_thread::sleep_for(chrono::milliseconds(speed));
+    }
+}
