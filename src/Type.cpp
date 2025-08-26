81e944aa28cb3618c24882ea7ffb5e247f1cd830
kcafernandes <kcafernandes06@gmail.com>
2025-08-26 16:36:01 -0700

    cleaned up type .h and .cpp

--- src/Type.cpp
+++ src/Type.cpp
@@ -3,11 +3,9 @@
 #include <thread>
 #include <chrono>
 
-using namespace std;
-
-void type(const string& text, int speed) {
-    for (unsigned int i = 0; i < text.length(); i++) {
-        cout << text[i] << flush;
-        this_thread::sleep_for(chrono::milliseconds(speed));
+void type(const std::string& text, int speed) {
+    for (std::size_t i = 0; i < text.length(); ++i) {
+        std::cout << text[i] << std::flush;
+        std::this_thread::sleep_for(std::chrono::milliseconds(speed));
     }
 }
