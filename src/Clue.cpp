4684e4c86e750a8710fbac65b3c39b97b522c1c1
kcafernandes <kcafernandes06@gmail.com>
2025-08-26 16:19:56 -0700

    updated clue .h and .cpp format and functions

--- src/Clue.cpp
+++ src/Clue.cpp
@@ -1,15 +1,34 @@
 #include "Clue.h"
 #include <iostream>
 
-Clue::Clue(const std::string name, const std::string description, const std::string location)
+Clue::Clue(const std::string& name,
+           const std::string& description,
+           const std::string& location)
     : name(name), description(description), location(location) {}
 
+// prints the clue in easy to read format for user access
 void Clue::printClue() const {
-    std::cout << name << " (" << location << "): " << description << "\n";
+    std::cout << name << " (" << location << "): " << description << '\n';
 }
 
-std::string Clue::getName() const { return name; }
-std::string Clue::getLocation() const { return location; }
+const std::string& Clue::getName() const noexcept {
+    return name;
+}
 
-void Clue::printCountdown() { }
-bool Clue::useClue() { return true; }
+const std::string& Clue::getLocation() const noexcept {
+    return location;
+}
+
+const std::string& Clue::getDescription() const noexcept {
+    return description;
+}
+
+void Clue::printCountdown() {
+    // TODO: implement countdown 
+}
+
+bool Clue::useClue() {
+    // TODO
+    return true;
+}
+
