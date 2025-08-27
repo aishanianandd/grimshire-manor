9541559d0b29e628c84cf0c3d9aba022d6b21879
ashita-816 <ashitasrini06@gmail.com>
2025-08-27 13:37:42 -0700

    cleaning up repo

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/csmajs/asrin045/final-project-tuffyteam
-# Build directory: /home/csmajs/asrin045/final-project-tuffyteam/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(AllTests "/home/csmajs/asrin045/final-project-tuffyteam/build/runtests")
-set_tests_properties(AllTests PROPERTIES  _BACKTRACE_TRIPLES "/home/csmajs/asrin045/final-project-tuffyteam/CMakeLists.txt;35;add_test;/home/csmajs/asrin045/final-project-tuffyteam/CMakeLists.txt;0;")
-subdirs("googletest")
--- build/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/csmajs/asrin045/final-project-tuffyteam/googletest
-# Build directory: /home/csmajs/asrin045/final-project-tuffyteam/build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
