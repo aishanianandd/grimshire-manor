44d16805c85015655a26b4bed23041957ed7cd1e
aishanianandd <aishaniianandd821@gmail.com>
2025-08-26 17:47:10 -0700

    added .h files for all locations

---
+++ include/Locations/Bedroom.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Location.h"
+
+// Bedroom class inherits from Location
+class Bedroom : public Location {
+public:
+    Bedroom();  // constructor
+};
---
+++ include/Locations/Library.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Location.h"
+
+// Library class inherits from Location
+class Library : public Location {
+public:
+    Library();  // constructor
+};
